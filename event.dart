701a6145e166292928004743d06c7f89d416ea3e
lee-yoo-jung <iy20344t@gmail.com>
2025-11-21 18:59:28 +0900

    "calenderview.dart schedule_bottom_sheet.dart TimePicker.dart detailplan.dart prints.dart" 추가 변경 파일

--- event.dart
+++ event.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 // 계획 클래스
 class Plan {
   final String text;         // 계획 내용
@@ -14,12 +16,18 @@ class Event {
   final String title;
   final DateTime startDate;
   final DateTime endDate;
+  Color color; // color 클래스 추가
+  bool secret;
+  List<String> togeter;
   List<Plan> plans = [];  //이벤트 속 계획 리스트, 일단 초기화
 
   Event({
     required this.title,
     required this.startDate,
     required this.endDate,
+    required this.togeter,
+    required this.color, // color 추가
+    required this.secret,
     List<Plan>? plans,
   }): plans = plans ?? [];
 
